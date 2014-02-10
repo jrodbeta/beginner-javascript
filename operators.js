e4ca091a4f75e5e0c264d1054b2e9eb08c4ddbec
Jose Rodriguez <jose@meetup.com>
2014-02-09 20:01:42 -0500

    Added operators example.

---
+++ operators.js
@@ -0,0 +1,9 @@
+var label = "Year";
+
+var ourYear1 = "Year: " + 2000 + 14;
+console.log("ourYear1 == " + ourYear1);
+
+var ourYear2 = "Year: " + ( 2000 + 14 );
+console.log("ourYear2 == " + ourYear2);
+
+console.log("Equal? " + ourYear1 == ourYear2);
